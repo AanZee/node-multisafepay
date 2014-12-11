131e1b99a8b9b463f2f7b5db48efd54e22bca106
Jelle Kralt <jelle@jellekralt.nl>
2014-12-11 15:13:08 +0100

    Added Redirect Transaction function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 /** Dependencies */
 var querystring = require('querystring'),
     request = require('request'),
+    md5 = require('MD5'),
     parseString = require('xml2js').parseString,
     _ = require('lodash');
 
@@ -17,7 +18,8 @@ var Client = function(options) {
     site_id: '',
     site_secure_code: '',
     env: 'production',
-    returnType: 'object'
+    returnType: 'object',
+    userAgent: 'node-multisafepay-0.1.1'
   };
 
   this.options = _.merge({}, defaults, options);
@@ -61,7 +63,7 @@ Client.prototype.post = function(body, callback) {
       body: body
     }, function(err, res, data) {
       if(err) {
-        throw err;
+        res.json(err);
       }
 
       switch(_this.options.returnType) {
@@ -100,7 +102,7 @@ Client.prototype.gateways = function(country, locale, callback) {
 
   body = ''+
   '<?xml version="1.0" encoding="utf-8"?>'+
-  '<gateways ua="node-multisafepay-0.1.0">'+
+  '<gateways ua="'+this.options.userAgent+'">'+
     '<merchant>'+
       '<account>'+this.options.account+'</account>'+
       '<site_id>'+this.options.site_id+'</site_id>'+
@@ -124,7 +126,7 @@ Client.prototype.idealissuers = function(callback) {
 
   body = ''+
   '<?xml version="1.0" encoding="utf-8"?>'+
-  '<idealissuers ua="node-multisafepay-0.1.0">'+
+  '<idealissuers ua="'+this.options.userAgent+'">'+
     '<merchant>'+
       '<account>'+this.options.account+'</account>'+
       '<site_id>'+this.options.site_id+'</site_id>'+
@@ -135,3 +137,56 @@ Client.prototype.idealissuers = function(callback) {
   this.post(body, callback);
 };
 
+Client.prototype.redirecttransaction = function(attributes, callback) {
+  var signature = md5(attributes.transaction.amount + attributes.transaction.currency + this.options.account + this.options.site_id + attributes.transaction.id);
+  var body;
+
+  body = ''+
+  '<?xml version="1.0" encoding="utf-8"?>'+
+  '<redirecttransaction ua="'+this.options.userAgent+'">'+
+    '<merchant>'+
+      '<account>'+this.options.account+'</account>'+
+      '<site_id>'+this.options.site_id+'</site_id>'+
+      '<site_secure_code>'+this.options.site_secure_code+'</site_secure_code>'+
+      '<notification_url>'+attributes.merchant.notification_url+'</notification_url>'+
+      '<redirect_url>'+attributes.merchant.redirect_url+'</redirect_url>'+
+      '<cancel_url>'+attributes.merchant.cancel_url+'</cancel_url>'+
+      '<close_window>'+attributes.merchant.close_window+'</close_window>'+
+    '</merchant>'+
+    '<customer>'+
+      '<locale>'+attributes.customer.locale+'</locale>'+
+      '<ipaddress>'+attributes.customer.ipaddress+'</ipaddress>'+
+      '<forwardedip>'+attributes.customer.forwardedip+'</forwardedip>'+
+      '<firstname>'+attributes.customer.firstname+'</firstname>'+
+      '<lastname>'+attributes.customer.lastname+'</lastname>'+
+      '<address1>'+attributes.customer.address1+'</address1>'+
+      '<address2>'+attributes.customer.address2+'</address2>'+
+      '<housenumber>'+attributes.customer.housenumber+'</housenumber>'+
+      '<zipcode>'+attributes.customer.zipcode+'</zipcode>'+
+      '<city>'+attributes.customer.city+'</city>'+
+      '<state>'+attributes.customer.state+'</state>'+
+      '<country>'+attributes.customer.country+'</country>'+
+      '<phone>'+attributes.customer.phone+'</phone>'+
+      '<email>'+attributes.customer.email+'</email>'+
+    '</customer>'+
+    '<transaction>'+
+      '<id>'+attributes.transaction.id+'</id>'+
+      '<currency>'+attributes.transaction.currency+'</currency>'+
+      '<amount>'+attributes.transaction.amount+'</amount>'+
+      '<description>'+attributes.transaction.description+'</description>'+
+      '<var1>'+attributes.transaction.var1+'</var1>'+
+      '<var2>'+attributes.transaction.var2+'</var2>'+
+      '<var3>'+attributes.transaction.var3+'</var3>'+
+      '<items>'+attributes.transaction.items+'</items>'+
+      '<manual>'+attributes.transaction.manual+'</manual>'+
+      '<gateway>'+attributes.transaction.gateway+'</gateway>'+
+      '<daysactive>'+attributes.transaction.daysactive+'</daysactive>'+
+    '</transaction>'+
+    '<google_analytics>'+
+      '<account>'+attributes.google_analytics.account+'</account>'+
+    '</google_analytics>'+
+    '<signature>'+(attributes.signature || signature)+'</signature>'+
+  '</redirecttransaction>';
+
+  this.post(body, callback);
+};
