c5cd64e91ce8417a313bd83855a2cd1a1c231ea3
Jelle Kralt <jelle@jellekralt.nl>
2014-12-12 10:12:35 +0100

    1upped semver

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var Client = function(options) {
     site_secure_code: '',
     env: 'production',
     returnType: 'object',
-    userAgent: 'node-multisafepay-0.1.3'
+    userAgent: 'node-multisafepay-0.1.4'
   };
 
   this.options = _.merge({}, defaults, options);
